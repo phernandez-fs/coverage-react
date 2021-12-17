b83767a4d016d2b0b399594bd6b0d2396a39709f
Pablo Hernández <phernandez@financialsolutions.com>
2021-12-17 10:17:14 -0600

    chenge in tests

---
+++ src/components/Bye.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Bye = () => {
+    return (
+        <div>
+            <h1>Bye from test component</h1>
+        </div>
+    )
+}
+
+export default Bye
--- src/test/Hello.test.js
+++ src/test/Hello.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import Hello from './src/components/Hello';
+import Hello from '../components/Hello';
 
 
 test('renders learn react link', () => {
