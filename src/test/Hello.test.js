729bb2e4df1dce39583c57263de589e887c9d615
Pablo Hernández <phernandez@financialsolutions.mx>
2022-02-14 12:54:08 -0600

    change in test

--- src/test/Hello.test.js
+++ src/test/Hello.test.js
@@ -4,7 +4,7 @@ import Hello from '../components/Hello';
 
 test('renders learn react link', () => {
     render(<Hello />);
-    const linkElement = screen.getByText("Hello from test component");
+    const linkElement = screen.getByText("Test crash");
     expect(linkElement).toBeInTheDocument();
   });
   
